6e33a7b9be6e8482f6ed94a2a2b973b5440b4870
casio25 <mishakolomiets355@gmail.com>
2023-03-12 21:23:19 +0200

    fixed location and magic number

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 const numberOfClients = 10;
-const authorName = ["Emily Rodriguez", "Thomas Nguyen", "Samantha Patel", "William Kim",
+const numberOfRooms = {
+    MIN: 1,
+    MAX: 10
+}
+const numberOfGuests = {
+    MIN: 1,
+    MAX: 50
+}
+const priceList = {
+    MIN: 300,
+    MAX: 1200
+}
+const numberOfAvatars = {
+    MIN: 1,
+    MAX: 8
+}
+const locationXValues = {
+    MIN: 35.65000,
+    MAX: 35.70000
+}
+const locationYValues = {
+    MIN: 139.70000,
+    MAX: 139.80000
+}
+const authorNames = ["Emily Rodriguez", "Thomas Nguyen", "Samantha Patel", "William Kim",
 "Elizabeth Davis", "Michael Johnson", "Rachel Lee", "Alexander Brown", "Olivia Garcia", "Benjamin Wright"]
 const checkinTimes = ["12:00", "13:00", "14:00"];
 const checkoutTimes = ["12:00", "13:00", "14:00"];
@@ -43,9 +67,11 @@ const roomDescriptions = [
     "Quaint room with a four-poster queen-size bed, hardwood floors, and a private bathroom.",
     "Family-friendly suite with bunk beds, a separate dining area, and easy access to nearby attractions."
 ];
+const photosArray = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const locationData = {};
 
 function shuffle(a) {
-    var j, x, i;
+    let j, x, i;
     for (i = a.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
         x = a[i];
@@ -65,32 +91,38 @@ function RandomFloatNumber(min, max){
 }
 
 
-const offers = new Array(numberOfClients).fill(null).map((e) => {
+const offers = new Array(numberOfClients).fill(null).map(() => {
     return getOffer();
 });
 
 function getOffer() {
     const shuffledFeatures = shuffle(features);
+    const getRandomX = RandomFloatNumber(locationXValues.MIN, locationXValues.MAX);
+    const getRandomY = RandomFloatNumber(locationYValues.MIN, locationYValues.MAX);
     return{
         author:{
-            avatar: `img/avatars/user0${RandomNaturalNumber(1, 8)}.png`
+            avatar: `img/avatars/user0${RandomNaturalNumber(numberOfAvatars.MIN, numberOfAvatars.MAX)}.png`
         },
         offer: {
             title: titleArray[RandomNaturalNumber(0, titleArray.length - 1)],
-            address: '{{location.x}}, {{location.y}}',
-            price: RandomNaturalNumber(300, 1200),
+            address: {
+                getRandomX,
+                getRandomY
+            },
+            price: RandomNaturalNumber(priceList.MIN, priceList.MAX),
             type: typeArray[RandomNaturalNumber(0, typeArray.length - 1)],
-            rooms: RandomNaturalNumber(1, 10),
-            guests: RandomNaturalNumber(1, 50),
+            rooms: RandomNaturalNumber(numberOfRooms.MIN, numberOfRooms.MAX),
+            guests: RandomNaturalNumber(numberOfGuests.MIN, numberOfGuests.MAX),
             checkin: checkinTimes[RandomNaturalNumber(0, checkinTimes.length - 1)],
             checkout: checkoutTimes[RandomNaturalNumber(0, checkoutTimes.length-1)],
             features:  shuffledFeatures.slice(0, RandomNaturalNumber(1, features.length -1)),
             description: roomDescriptions[RandomNaturalNumber(0, roomDescriptions.length -1)],
-            photos: [],
+            photos: photosArray.slice(0, RandomNaturalNumber(1,photosArray.length-1)),
             location:{
-                x: RandomFloatNumber(35.65000, 35.70000),
-                y: RandomFloatNumber(139.70000, 139.80000),
-            }
+                x: getRandomX,
+                y: getRandomY,
+            },
         }
     }
 }
+console.log(offers);
