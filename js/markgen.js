33f088eb7867bd4be263a25087653f9006dcd69c
casio25 <mishakolomiets355@gmail.com>
2023-03-13 22:49:28 +0200

    icons

--- js/markgen.js
+++ js/markgen.js
@@ -10,7 +10,12 @@ const popupPrice = cardTemplate.content.querySelector(".popup__text--price");
 const popupType = cardTemplate.content.querySelector(".popup__type");
 const popupCapacity = cardTemplate.content.querySelector(".popup__text--capacity");
 const popupTime = cardTemplate.content.querySelector(".popup__text--time");
-const popupFeatures = cardTemplate.content.querySelector(".popup__features");
+const popupWifi = cardTemplate.content.querySelector(".popup__feature--wifi");
+const popupDishwasher = cardTemplate.content.querySelector(".popup__feature--dishwasher");
+const popupWasher = cardTemplate.content.querySelector(".popup__feature--washer");
+const popupParking = cardTemplate.content.querySelector(".popup__feature--parking");
+const popupElevator = cardTemplate.content.querySelector(".popup__feature--elevator");
+const popupConditioner = cardTemplate.content.querySelector(".popup__feature--conditioner");
 const popupDescription = cardTemplate.content.querySelector(".popup__description");
 const popupPhotos = cardTemplate.content.querySelector(".popup__photos");
 export function showPopup(){
@@ -18,7 +23,24 @@ export function showPopup(){
 
 offers.map((e) => checkForUndefined(e));
 
-
+function getFeatures() {
+    for(let i = 0; i < offers.length; i++){
+        if (!offers[i].offer.features.includes("wifi")){
+            popupWifi.classList.add("hidden")
+        } else if (!offers[i].offer.features.includes("dishwasher")){
+            popupDishwasher.classList.add("hidden")
+        } else if (!offers[i].offer.features.includes("washer")){
+            popupWasher.classList.add("hidden")
+        } else if (!offers[i].offer.features.includes("parking")){
+            popupParking.classList.add("hidden")
+        } else if (!offers[i].offer.features.includes("elevator")){
+            popupElevator.classList.add("hidden")
+        } else if (!offers[i].offer.features.includes("conditioner")){
+            popupConditioner.classList.add("hidden")
+        }
+    }
+    
+}
 function showPhotos(listOfPhotos){
     for(let i = 0; i < listOfPhotos.length; i++){
         const e = document.createElement("img");
@@ -47,7 +69,7 @@ function getOfferData(e) {
     popupPrice.textContent = `${e.offer.price} грн/ніч`;
     popupCapacity.textContent = `${e.offer.rooms} кімнат для ${e.offer.guests} гостей`;
     popupTime.textContent = `Заїзд після ${e.offer.checkin} виїзд до ${e.offer.checkout}`;
-    popupFeatures.textContent = e.offer.features;
+    getFeatures();
     popupDescription.textContent = e.offer.description;
     showPhotos(e.offer.photos);
 
