168116b2e92f3bebae7218c93482b560b48f1b11
casio25 <mishakolomiets355@gmail.com>
2023-03-18 19:11:07 +0200

    added disable

--- js/main.js
+++ js/main.js
@@ -8,12 +8,10 @@ console.log(offers);
 let form = document.querySelector(".ad-form");
 let allElements = form.elements;
 for (let i = 0, l = allElements.length; i < l; ++i) {
-    // allElements[i].readOnly = true;
     allElements[i].disabled = true;
 }
 function pageIsLoaded() {
     for (let i = 0, l = allElements.length; i < l; ++i) {
-        // allElements[i].readOnly = true;
         allElements[i].disabled = false;
     }
 }
